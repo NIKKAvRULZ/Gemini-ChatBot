c17132d0232b1d83cd08dd05bbbf008870be14f3
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-07-10 10:13:58 +0530

    fix import typo and update model instantiation in app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-import google.genaretiveai as genai
-
+import google.generativeai as genai
 API_KEY = "AIzaSyCKkCWBOMQhckvf6u-ctkUeMfbo7fpueEY"
 
 genai.configure(api_key = API_KEY)
 
-model = genai.Model("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 chat = model.start_chat()
 
-print("Welcome to the Gemini 2.0 Flash model chat!")
+response = chat.send_message("What is the capital of France?")
+
+print("Gemini : ",response.text)
